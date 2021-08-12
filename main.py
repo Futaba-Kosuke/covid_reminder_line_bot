1572d841124b064ef31cea20bc06ec3c18d9962a
WhiteF326 <Lume1a326.leno@gmail.com>
2021-08-12 21:54:15 +0900

    Update: エラーメッセージとエラー処理、コメントの追加

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import sys
 from typing import Literal
 
 import uvicorn
-from fastapi import FastAPI, Request, BackgroundTasks
-from linebot import WebhookParser
+from fastapi import FastAPI, Request, Response, BackgroundTasks
+from linebot import WebhookParser, exceptions
 from linebot.models import TextMessage
 from aiolinebot import AioLineBotApi
 
 # get token and secret from os environment
 if not (line_token := os.environ.get(
     "POETRY_COVID19_REMINDER_LINE_BOT_TOKEN")):
-    sys.exit("")
+    sys.exit("Environment variable not found ‘LINE_BOT_TOKEN’")
 if not (line_secret := os.environ.get(
-    "POETRY_COVID19_REMINDER_LINE_BOT_CHANNEL_SECRET")):
-    sys.exit("")
+    "POETRY_COVID19_REMINDER_LINE_CHANNEL_SECRET")):
+    sys.exit("Environment variable not found ‘LINE_CHANNEL_SECRET’")
 
 # create line api client
 line_api = AioLineBotApi(channel_access_token=line_token)
@@ -30,6 +30,7 @@ def main():
     uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True, workers=2)
 
 
+# body of echo
 async def echo_body(event):
     await line_api.reply_message_async(
         event.reply_token,
@@ -39,18 +40,24 @@ async def echo_body(event):
 
 @app.post("/messaging_api/echo")
 async def echo(
-    request: Request, background_tasks: BackgroundTasks) -> Literal['ok']:
+    request: Request, background_tasks: BackgroundTasks):
     # parse request and get events
-    events = parser.parse(
-        (await request.body()).decode("utf-8"),
-        request.headers.get("X-Line-Signature", "")
-    )
+    try:
+        events = parser.parse(
+            (await request.body()).decode("utf-8"),
+            request.headers.get("X-Line-Signature", "")
+        )
+    except exceptions.InvalidSignatureError:
+        print("Error: failed to parse events")
+        # return 503
+        return Response(content="Internal Server Error", status_code=503)
 
     # process each event
     for ev in events:
         background_tasks.add_task(echo_body, event = ev)
-    
-    return "ok"
+
+    # return response
+    return Response(content="OK", status_code=200)
 
 
 if __name__ == '__main__':
