2a6694c05ebe8472224e4e535596a2c917227549
WhiteF326 <Lume1a326.leno@gmail.com>
2021-08-12 21:16:42 +0900

    Update: background_tasksを使用する形に変更 (#2)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from typing import Literal
 
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, BackgroundTasks
 from linebot import WebhookParser
 from linebot.models import TextMessage
 from aiolinebot import AioLineBotApi
@@ -30,8 +30,16 @@ def main():
     uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True, workers=2)
 
 
+async def echo_body(event):
+    await line_api.reply_message_async(
+        event.reply_token,
+        TextMessage(text=f"{event.message.text}")
+    )
+    pass
+
 @app.post("/messaging_api/echo")
-async def echo(request: Request) -> Literal['ok']:
+async def echo(
+    request: Request, background_tasks: BackgroundTasks) -> Literal['ok']:
     # parse request and get events
     events = parser.parse(
         (await request.body()).decode("utf-8"),
@@ -40,10 +48,7 @@ async def echo(request: Request) -> Literal['ok']:
 
     # process each event
     for ev in events:
-        await line_api.reply_message_async(
-            ev.reply_token,
-            TextMessage(text=f"{ev.message.text}")
-        )
+        background_tasks.add_task(echo_body, event = ev)
     
     return "ok"
 
