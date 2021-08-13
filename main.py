c528b9fbf0912d2893926aae73a1a4df1776891b
白羽 / 三田嘉月 <54446617+WhiteF326@users.noreply.github.com>
2021-08-13 21:08:33 +0900

    Clean: 提案された変更の取り入れNo.4 (#2)
    Co-authored-by: niba2828 <49780545+Futaba-Kosuke@users.noreply.github.com>

--- main.py
+++ main.py
@@ -30,6 +30,10 @@ app = FastAPI()
 
 def main():
     uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True, workers=2)
+    
+    
+if __name__ == '__main__':
+    main()
 
 
 # body of echo
