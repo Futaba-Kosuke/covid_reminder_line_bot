7889566fcf67f4ce37af1f6e8bd882f4cdb5a687
WhiteF326 <Lume1a326.leno@gmail.com>
2021-08-30 22:08:49 +0900

    Add: DB接続
    test.py によりテスト実施

--- db_connector/main.py
+++ db_connector/main.py
@@ -1,25 +1,21 @@
 import os
+from typing import Final
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from google.cloud.firestore_v1 import Client
 from google.cloud.firestore_v1.collection import CollectionReference
-from google.cloud.firestore_v1.base_document import DocumentSnapshot
 
-from covid_data_getter import prefectures_dict
+FIREBASE_CRED_ID: Final[str] = os.getenv("COVID19_REMINDER_FIREBASE_CRED_ID")
 
-FIREBASE_CRED_ID = os.getenv("COVID19_REMINDER_FIREBASE_CRED_ID")
 
-cred: credentials.Certificate \
-    = credentials.Certificate(os.path.dirname(__file__)
-                              + "\\covid-reminder-line-bot-firebase-adminsdk-"
-                              + FIREBASE_CRED_ID + ".json")
-app: firebase_admin.App = firebase_admin.initialize_app(cred)
+def get_reference(collection_name: str) -> CollectionReference:
+    cred: credentials.Certificate \
+        = credentials.Certificate(os.path.dirname(__file__)
+                                  + "\\covid-reminder-line-bot-firebase-adminsdk-"
+                                  + FIREBASE_CRED_ID + ".json")
+    app: firebase_admin.App = firebase_admin.initialize_app(cred)
 
-db: Client = firestore.client(app)
-ref: CollectionReference = db.collection(u'prefectures')
-docs = ref.stream()
-
-for id, name in list(prefectures_dict.items())[1:]:
-    ref.add({"id": id, "name": name})
+    db: Client = firestore.client(app)
+    return db.collection(collection_name)
---
+++ db_connector/test.py
@@ -0,0 +1,7 @@
+from db_connector.main import get_reference, CollectionReference
+
+ref: CollectionReference = get_reference("prefectures")
+docs = ref.stream()
+
+for doc in docs:
+    print(doc.to_dict())
