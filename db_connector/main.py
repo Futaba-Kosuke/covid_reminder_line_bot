8003ee1c71eb27f2b57ea83814588fd0967e1b5a
WhiteF326 <Lume1a326.leno@gmail.com>
2021-08-31 21:10:14 +0900

    Add: DB 更新部分の作成 (#4)

--- db_connector/main.py
+++ db_connector/main.py
@@ -7,15 +7,40 @@ from firebase_admin import firestore
 from google.cloud.firestore_v1 import Client
 from google.cloud.firestore_v1.collection import CollectionReference
 
-FIREBASE_CRED_ID: Final[str] = os.getenv("COVID19_REMINDER_FIREBASE_CRED_ID")
 
+class Firebase:
+    FIREBASE_CRED_ID: Final[str] = os.getenv("COVID19_REMINDER_FIREBASE_CRED_ID")
 
-def get_reference(collection_name: str) -> CollectionReference:
     cred: credentials.Certificate \
         = credentials.Certificate(os.path.dirname(__file__)
                                   + "\\covid-reminder-line-bot-firebase-adminsdk-"
                                   + FIREBASE_CRED_ID + ".json")
-    app: firebase_admin.App = firebase_admin.initialize_app(cred)
+    firebase_app: firebase_admin.App = firebase_admin.initialize_app(cred)
+    db: Client = firestore.client(firebase_app)
 
-    db: Client = firestore.client(app)
-    return db.collection(collection_name)
+    def get_reference(self, collection_name: str) -> CollectionReference:
+        return self.db.collection(collection_name)
+
+    def register_user(self, user_id: str, prefecture_id: str):
+        users_collection = self.db.collection("users")
+        users_collection.add({"userid": user_id, "prefectureid": prefecture_id})
+
+    def update_user_prefecture_id(self, user_id: str, prefecture_id: str):
+        users_collection = self.db.collection("users")
+        users_doc = list(users_collection.where(u'userid', u'==', user_id).stream())[0]
+        users_doc_dict = users_doc.to_dict()
+
+        users_doc_dict["prefectureid"] = prefecture_id
+        self.db.collection("users").document(users_doc.id).set(users_doc_dict)
+
+    def update_prefecture_userid_list(self, prefecture_id: str, user_id: str, action='add'):
+        prefecture_collection = self.db.collection("prefectures")
+        prefecture_doc = list(prefecture_collection.where(u'id', u'==', prefecture_id).stream())[0]
+        prefecture_doc_dict = prefecture_doc.to_dict()
+
+        if action == 'add':
+            prefecture_doc_dict['userid'].append(user_id)
+        elif action == 'remove':
+            prefecture_doc_dict['userid'].remove(user_id)
+
+        self.db.collection("prefectures").document(prefecture_doc.id).set(prefecture_doc_dict)
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from aiolinebot import AioLineBotApi
 
 from covid_data_getter \
     import prefectures_dict, PatientsType, get_daily_patients, mock_get_target_prefectures
-from db_connector import get_reference
+from db_connector import Firebase
 
 LINE_ACCESS_TOKEN: Final[str] = os.getenv('COVID19_REMINDER_LINE_ACCESS_TOKEN')
 LINE_CHANNEL_SECRET: Final[str] = os.getenv('COVID19_REMINDER_LINE_CHANNEL_SECRET')
@@ -31,27 +31,48 @@ parser = WebhookParser(channel_secret=LINE_CHANNEL_SECRET)
 # startup FastAPI
 app = FastAPI()
 
+# firebase Instance
+firebase = Firebase()
+
 
 # body of echo
-async def echo_body(event: LineTextMessageEventType, message_text: str) -> NoReturn:
-    message_text = message_text.split()
+async def echo_body(event: LineTextMessageEventType, user_id: str) -> NoReturn:
+    message_text = event.message.text.split()
     if message_text[0] == "登録" and len(message_text) >= 1:
-        prefectures_list = {"全国" : "ALL"}
-        prefectures_list |= ((name, id) for id, name in list(prefectures_dict.items())[1:])
-        prefectures_list |= ((name[:-1], id) for id, name in list(prefectures_dict.items())[1:])
+        # registration
+        prefectures_list = {"全国": "ALL"}
+        prefectures_list |= ((name, prefecture_id) for prefecture_id, name in list(prefectures_dict.items())[1:])
+        prefectures_list |= ((name[:-1], prefecture_id) for prefecture_id, name in list(prefectures_dict.items())[1:])
+
         if message_text[1] in prefectures_list.keys():
-            # 
-            # update prefectures collection
-            prefectures_ref = get_reference("prefectures")
-            
-            print(prefectures_list[message_text[1]])
+            # able to register, get previous prefecture
+            new_prefecture_id = prefectures_list[message_text[1]]
+            users_ref = firebase.get_reference("users")
+            search_user = list(users_ref.where(u'userid', u'==', user_id).stream())
+
+            if len(search_user):
+                previous_prefecture_id = search_user[0].to_dict()['prefectureid']
+                firebase.update_prefecture_userid_list(previous_prefecture_id, user_id, 'remove')
+                firebase.update_user_prefecture_id(user_id, new_prefecture_id)
+            else:
+                # register
+                firebase.register_user(user_id, prefectures_list[message_text[1]])
+                await line_api.reply_message_async(
+                    event.reply_token,
+                    TextMessage(text="都道府県の登録が完了しました。")
+                )
+
+            firebase.update_prefecture_userid_list(new_prefecture_id, user_id, 'add')
+
         else:
+            # not able to register
             await line_api.reply_message_async(
                 event.reply_token,
                 TextMessage(text="都道府県名の指定が正しくありません。")
             )
         pass
     else:
+        # send today's number of new infected
         daily_patients: PatientsType = get_daily_patients()
         target_prefectures: List[str] = mock_get_target_prefectures()
 
@@ -80,7 +101,7 @@ async def echo(request: Request, background_tasks: BackgroundTasks) -> Response:
     # process each event
     for request_event in request_events:
         if isinstance(request_event.message, TextMessage):
-            background_tasks.add_task(echo_body, event=request_event, message_text=request_event.message.text)
+            background_tasks.add_task(echo_body, event=request_event, user_id=request_event.source.user_id)
 
     # return response
     return Response(content="OK", status_code=200)
