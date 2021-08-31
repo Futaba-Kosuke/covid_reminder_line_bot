e7e9b87e948b64390df6d78c3a4a76703cedeb4d
WhiteF326 <Lume1a326.leno@gmail.com>
2021-08-31 17:10:18 +0900

    Update: DB接続モジュールのリファクタリング

--- db_connector/test.py
+++ db_connector/test.py
@@ -1,7 +1,3 @@
-from main import get_reference, CollectionReference
+from db_connector.main import get_reference, CollectionReference
 
 ref: CollectionReference = get_reference("prefectures")
-docs = ref.stream()
-
-for doc in docs:
-    print(doc.to_dict())
