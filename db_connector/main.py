dea29887b65b9e716c9dff1f9a44f967fb03af60
WhiteF326 <Lume1a326.leno@gmail.com>
2021-08-31 23:53:21 +0900

    Add: 登録された都道府県に応じての返答を追加 (#4)
    db_connector\main.pyのコンストラクタが2回呼ばれる原因不明のバグあり 暫定処置済み

--- db_connector/main.py
+++ db_connector/main.py
@@ -1,29 +1,37 @@
 import os
-from typing import Final
+import random
+from typing import Final, NoReturn
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from google.cloud.firestore_v1 import Client
-from google.cloud.firestore_v1.collection import CollectionReference
 
 
 class Firebase:
-    FIREBASE_CRED_ID: Final[str] = os.getenv("COVID19_REMINDER_FIREBASE_CRED_ID")
 
-    cred: credentials.Certificate \
-        = credentials.Certificate(os.path.dirname(__file__)
-                                  + "\\covid-reminder-line-bot-firebase-adminsdk-"
-                                  + FIREBASE_CRED_ID + ".json")
-    firebase_app: firebase_admin.App = firebase_admin.initialize_app(cred)
-    db: Client = firestore.client(firebase_app)
+    def __init__(self) -> NoReturn:
+        FIREBASE_CRED_ID: Final[str] = os.getenv("COVID19_REMINDER_FIREBASE_CRED_ID")
+        cred: credentials.Certificate \
+            = credentials.Certificate(os.path.dirname(__file__)
+                                      + "\\covid-reminder-line-bot-firebase-adminsdk-"
+                                      + FIREBASE_CRED_ID + ".json")
+        self.db: Client = firestore.client(
+            firebase_admin.initialize_app(cred, name=str(random.random())))
 
-    def get_reference(self, collection_name: str) -> CollectionReference:
-        return self.db.collection(collection_name)
+    def get_user_prefecture(self, user_id: str) -> str:
+        users_collection = self.db.collection("users")
+        return \
+            list(users_collection.where(u'userid', u'==', user_id).stream())[0] \
+            .to_dict()['prefectureid']
 
     def register_user(self, user_id: str, prefecture_id: str):
         users_collection = self.db.collection("users")
         users_collection.add({"userid": user_id, "prefectureid": prefecture_id})
+
+    def search_user(self, user_id: str):
+        users_collection = self.db.collection("users")
+        return list(users_collection.where(u'userid', u'==', user_id).stream())
 
     def update_user_prefecture_id(self, user_id: str, prefecture_id: str):
         users_collection = self.db.collection("users")
@@ -33,7 +41,7 @@ class Firebase:
         users_doc_dict["prefectureid"] = prefecture_id
         self.db.collection("users").document(users_doc.id).set(users_doc_dict)
 
-    def update_prefecture_userid_list(self, prefecture_id: str, user_id: str, action='add'):
+    def update_prefecture_userid_list(self, prefecture_id: str, user_id: str, action: str):
         prefecture_collection = self.db.collection("prefectures")
         prefecture_doc = list(prefecture_collection.where(u'id', u'==', prefecture_id).stream())[0]
         prefecture_doc_dict = prefecture_doc.to_dict()
